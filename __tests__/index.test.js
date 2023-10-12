07a338bd1062a0a736ac59d9a99d056c1b7df7d2
sseezov <sseezov@mail.ru>
2023-10-12 19:54:59 +0700

    4 tests steps done

---
+++ __tests__/index.test.js
@@ -0,0 +1,49 @@
+// @ts-check
+
+import { test } from 'node:test';
+import assert from 'assert/strict';
+import Validator from '../index.js';
+
+test('task1', () => {
+  const validator = new Validator();
+  const schema = validator.number();
+
+  assert.equal(schema.isValid(null), false); // false
+  assert.equal(schema.isValid(''), false); // false
+  assert.equal(schema.isValid(true), false); // false
+  assert.equal(schema.isValid(123), true); // true
+  assert.equal(schema.isValid(0), true); // true
+});
+
+test('task2', () => {
+  const validator = new Validator();
+  const schema1 = validator.number().even();
+  const schema2 = validator.number().odd();
+
+  assert.equal(schema1.isValid(''), false);
+  assert.equal(schema1.isValid(2), true);
+  assert.equal(schema2.isValid(2), false);
+  assert.equal(schema2.isValid(3), true);
+});
+
+test('task3', () => {
+  const validator = new Validator();
+  const schema = validator.array();
+
+  assert.equal(schema.isValid('Hello!'), false);
+  assert.equal(schema.isValid(123), false);
+  assert.equal(schema.isValid([]), true);
+  assert.equal(schema.isValid([1, 23, 4]), true);
+});
+
+test('task4', () => {
+  const validator = new Validator();
+  const schema1 = validator.array().length(4);
+  const schema2 = validator.array().length(12312312455434);
+
+  assert.equal(schema1.isValid(null), false);
+  assert.equal(schema1.isValid([]), false);
+  assert.equal(schema1.isValid([1, 2, 3, 4]), true);
+  assert.equal(schema2.isValid([]), false);
+  assert.equal(schema2.isValid([1, 23, 4, 5]), false);
+});
