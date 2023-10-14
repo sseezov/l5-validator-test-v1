b83c567b9867c1aecda4b906aa783c6fb51e06bd
Goga-Rid <wopa22@list.ru>
2023-10-14 11:23:36 +0300

    [FIX] fix error eslint no-restricted-syntax

--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -1,32 +1,26 @@
 class ObjectSchrema {
-    constructor(value){
-        this.validations = {};
+  constructor() {
+    this.validations = {};
+  }
+
+  shape(newData) {
+    this.validations = newData;
+    return this;
+    // или через создание нового экземпляра return new ObjectSchrema(newData);
+  }
+
+  isValid(data) {
+    const dataKeys = Object.keys(data);
+    const schemaKeys = Object.keys(this.validations);
+
+    if (dataKeys.length !== schemaKeys.length) {
+      return false;
     }
 
-    shape(newData){
-        this.validations = newData;
-        return this;
-        // или через создание нового экземпляра return new ObjectSchrema(newData);
+    if (schemaKeys.every((key) => this.validations[key].isValid(data[key]))) {
+      return true;
     }
-
-    isValid(data){
-        const dataKeys = Object.keys(data);
-        const schemaKeys = Object.keys(this.validations);
-    
-        if (dataKeys.length !== schemaKeys.length) {
-          return false;
-        }
-    
-        for (const key of schemaKeys) {
-          const validation = this.validations[key];
-          if (!validation.isValid(data[key])) {
-            return false;
-          }
-        }
-    
-        return true;
-    }
-
-
+    return false;
+  }
 }
 export default ObjectSchrema;
