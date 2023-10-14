3375de1ea426ef8f3b3c0a8138a94299d523e929
Goga-Rid <wopa22@list.ru>
2023-10-14 11:16:23 +0300

    [FEATURE] complete 5 task

--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -1,4 +1,32 @@
 class ObjectSchrema {
+    constructor(value){
+        this.validations = {};
+    }
+
+    shape(newData){
+        this.validations = newData;
+        return this;
+        // или через создание нового экземпляра return new ObjectSchrema(newData);
+    }
+
+    isValid(data){
+        const dataKeys = Object.keys(data);
+        const schemaKeys = Object.keys(this.validations);
+    
+        if (dataKeys.length !== schemaKeys.length) {
+          return false;
+        }
+    
+        for (const key of schemaKeys) {
+          const validation = this.validations[key];
+          if (!validation.isValid(data[key])) {
+            return false;
+          }
+        }
+    
+        return true;
+    }
+
 
 }
 export default ObjectSchrema;
